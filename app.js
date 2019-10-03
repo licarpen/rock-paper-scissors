beff1150d1351beb080c640adec70635697e1629
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-10-03 10:26:42 -0700

    version 1.0 of paper rock scissors game

--- app.js
+++ app.js
@@ -2,35 +2,50 @@ import { generateRandomNumber, getThrowFromNumber, getWinner } from './utilities
 
 // get DOM elements
 
-const userPlay = document.getElementById("user-play");
 const userInput = document.getElementById("user-input");
 const computer = document.getElementById("computer-play");
 const winner = document.getElementById("winner");
 const playaRound = document.getElementById("play-round");
 const userScoreBoard = document.getElementById("user-score");
 const computerScoreBoard = document.getElementById("computer-score");
-const nextRound = document.getElementById("next-round");
 const startOver = document.getElementById("start-over");
 
 // initialize DOM state
 
 let userScore = 0;
 let computerScore = 0;
+let computerPlay;
+let userSelection;
+let champion;
+let selectedRadioButton;
+
+// change DOM state and elements based on game play
+
+const resetGame = () => {
+    userScore = 0;
+    computerScore = 0;
+    userScoreBoard.textContent = userScore;
+    computerScoreBoard.textContent = computerScore;
+    computer.textContent = '';
+    userInput.textContent = '';
+    winner.textContent = '';
+};
 
 const playRound = () => {
-    const randomNumber = generateRandomNumber();
+    let randomNumber = generateRandomNumber();
     console.log(randomNumber);
-    const computerPlay = getThrowFromNumber(randomNumber);
-    const selectedRadioButton = document.querySelector('input:checked');
+    computerPlay = getThrowFromNumber(randomNumber);
+    selectedRadioButton = document.querySelector('input:checked');
     console.log(selectedRadioButton);
-    const userSelection = selectedRadioButton.value;
+    userSelection = selectedRadioButton.value;
     console.log(userSelection);
+    champion = getWinner(userSelection, computerPlay);
+    updateSpans();  
+};
 
-    const champion = getWinner(userSelection, computerPlay)
-
-    // change into separate function: updateSpans()    
-    computer.textContent = `Computer played ${computerPlay}. `;
-    userInput.textContent = `You played ${userSelection}.`;
+const updateSpans = () => {  
+    computer.textContent = ` Computer played ${computerPlay}. `;
+    userInput.textContent = `You played ${userSelection}. `;
     if (champion === 'You'){
         winner.textContent = 'You win this round!';
         userScore++;
@@ -44,9 +59,8 @@ const playRound = () => {
     else {
         winner.textContent = 'It\'s a Draw.';
     }
-    
-}
+};
 
 // add event listeners
-
 playaRound.addEventListener('click', playRound);
+startOver.addEventListener('click', resetGame);
--- utilities.js
+++ utilities.js
@@ -23,3 +23,4 @@ export const getWinner = (userPlay, computerPlay) => {
     else
         return 'Computer';
 };
+
